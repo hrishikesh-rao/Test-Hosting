ceb2fe4f401506c45a1ffdbff9a6a34e7df333e7
hrishikesh-rao <hrishikeshrao6@gmail.com>
2026-08-21 19:52:47 +0530

    Add root route

--- Backend/server.js
+++ Backend/server.js
@@ -5,12 +5,19 @@ const app = express();
 
 app.use(cors());
 
+// Root route
+app.get("/", (req, res) => {
+    res.send("Backend is running!");
+});
+
+// API test route
 app.get("/api/hello", (req, res) => {
     res.json({
         message: "Backend is working!"
     });
 });
 
+// Port
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
